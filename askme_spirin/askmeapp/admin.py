5250be18b022815ca0a6e7a17dc1b4e992bb4798
Spirin Mihail <misha_spirin_90@mail.ru>
2023-04-10 00:19:51 +0300

    add admin

--- askme_spirin/askmeapp/admin.py
+++ askme_spirin/askmeapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+admin.site.register(models.Answer)
+admin.site.register(models.Tag)
+admin.site.register(models.Like)
+admin.site.register(models.AskmeUser)
+admin.site.register(models.Question)
