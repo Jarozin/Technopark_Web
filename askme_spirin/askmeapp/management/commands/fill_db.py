5b82bc29a65ba60c91d56c4b228d127d79777a97
Spirin Mihail <misha_spirin_90@mail.ru>
2023-04-10 20:30:23 +0300

    finish creating data

--- askme_spirin/askmeapp/management/commands/fill_db.py
+++ askme_spirin/askmeapp/management/commands/fill_db.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from askmeapp import models
+from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     help = "Add objects to databse according to ratio"
@@ -7,10 +8,47 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('ratio', type=int)
 
-
     def handle(self, *args, **options):
         ratio = options['ratio']
         i = 0
+        avatar_images = list()
+        for i in range(3):
+            with open(f"/home/jarozin/Technopark/Web/dz/askme_spirin/static/img/avatar{i + 1}.png", "rb") as image:
+                f = image.read()
+                avatar_images.append(bytearray(f))
+        new_bulk = list()
         for i in range(ratio):
             new_tag = models.Tag(name = f"Tag_{i}")
-            new_tag.save()
+            models.Tag.objects.bulk_create([new_tag])
+
+            new_user = User(username = f"User_{i}", email = f'user_{i}@mail.ru', password='asdqweqwe_123')
+            models.User.objects.bulk_create([new_user])
+
+            new_profile = models.Profile(user = new_user, avatar=avatar_images[i % 3])
+            models.Profile.objects.bulk_create([new_profile])
+
+
+            new_content = list()
+            for j in range(110):
+                new_common_content = models.CommonContent(user = new_profile)
+                new_content.append(new_common_content)
+            models.CommonContent.objects.bulk_create(new_content)
+            ###TODO: поменять коэф для лайков до 200
+            new_likes = list()
+            for j in range(20):
+                new_like = models.Like(user = new_profile, state=bool(j % 2), common_content = new_content[j * 3])
+                new_likes.append(new_like)
+            models.Like.objects.bulk_create(new_likes)
+
+            new_questions = list()
+            for j in range(10):
+                new_question = models.Question(title=f'{i * 10 + j} question title', content = f'this is a content for question number {i * 10 + j}', common_content = new_content[j])
+                new_questions.append(new_question)
+            models.Question.objects.bulk_create(new_questions)
+            for j in range(10):
+                new_questions[j].tag.set([new_tag])
+            new_answers = list()
+            for j in range(100):
+                new_answer = models.Answer(content = f'content for answer number {j + i * 100}', correct = not bool(j % 100), question = new_questions[j % 10], common_content = new_content[10 + j])
+                new_answers.append(new_answer)
+            models.Answer.objects.bulk_create(new_answers)
--- askme_spirin/askmeapp/models.py
+++ askme_spirin/askmeapp/models.py
@@ -27,46 +27,58 @@ QUESTIONS = [
 
 
 class Like(models.Model):
-    CommonContent = models.ForeignKey('CommonContent', on_delete=models.PROTECT, default=1)
+    common_content = models.ForeignKey(
+        'CommonContent', on_delete=models.PROTECT)
     state = models.BooleanField()
     user = models.ForeignKey('Profile', on_delete=models.CASCADE)
+
     def __str__(self):
         if (self.state):
             like_state = '+1'
         else:
             like_state = '-1'
         return str(self.user) + ': ' + like_state
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
 
 class CommonContent(models.Model):
-    user = models.OneToOneField('Profile', on_delete=models.CASCADE)
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE)
+
     def __str__(self):
         return str(self.user)
+
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    common_content = models.OneToOneField('CommonContent', on_delete=models.CASCADE, default=1)
-    like = models.ForeignKey('Like', on_delete=models.PROTECT)
+    common_content = models.OneToOneField(
+        'CommonContent', on_delete=models.CASCADE)
     tag = models.ManyToManyField('Tag')
+
     def __str__(self):
         return self.title
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     avatar = models.BinaryField()
+
     def __str__(self):
         return self.user.get_username()
+
 
 class Answer(models.Model):
     content = models.TextField()
     correct = models.BooleanField(default=False)
     question = models.ForeignKey('Question', on_delete=models.CASCADE)
-    common_content = models.OneToOneField('CommonContent', on_delete=models.CASCADE, default=1)
-    like = models.ForeignKey('Like', on_delete=models.PROTECT)
+    common_content = models.OneToOneField(
+        'CommonContent', on_delete=models.CASCADE)
+
     def __str__(self):
-        return self.content[:25]
+        return self.content[:40]
