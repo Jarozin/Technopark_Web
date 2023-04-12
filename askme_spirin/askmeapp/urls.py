712639ab5b71bab4081004a6c49aab7880c40f46
Spirin Mihail <misha_spirin_90@mail.ru>
2023-04-13 00:18:38 +0300

    separate routing

--- askme_spirin/askmeapp/urls.py
+++ askme_spirin/askmeapp/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from askmeapp import views
+
 urlpatterns = [
-    path('', include('askmeapp.urls')),
+    path('', views.index, name='index'),
+    path('question/<int:question_id>/', views.question, name="question"),
+    path('admin/', admin.site.urls),
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path('ask/', views.ask, name="ask"),
+    path('settings/', views.settings, name='settings'),
+    path('tag/<str:tag_name>/', views.tag, name="tag"),
+    path('hot/', views.hot, name="hot"),
 ]
